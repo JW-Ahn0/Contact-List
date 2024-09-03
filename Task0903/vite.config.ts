17eb3cbd98613d777e800b6d82917400934b998f
JW-Ahn0 <jun.woo.an0@gmail.com>
2024-09-03 16:51:59 +0900

    [Fix] 배포관련하여 vite 설정 파일 수정

--- Task0903/vite.config.ts
+++ Task0903/vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/{Contact-List}/", // {repo-name}을 GitHub 저장소 이름으로 변경
+  base: "/Contact-List/", // {repo-name}을 GitHub 저장소 이름으로 변경
 });
